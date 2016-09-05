efdad3da1b5efd121920f4a4a895f40aa3dd0da1
godka <mythkast@hotmail.com>
2016-09-05 12:48:38 +0800

    好像还是有点问题

--- SimpleMdMpi/SimpleMdMpi.cpp
+++ SimpleMdMpi/SimpleMdMpi.cpp
@@ -11,19 +11,25 @@
 
 using namespace std;
 #define TIMES 1000
-const int N = 640;         // number of particles
-double** r;           // positions
-double** v;           // velocities
-double** a;           // accelerations
+const int N = 64;         // number of particles
+double* r;           // positions
+double* v;           // velocities
+double* a;           // accelerations
 
 double L = 10;            // linear size of cubical volume
 double vMax = 0.1;        // maximum initial velocity component
+void setPtr(double* dst, int i, int j, double src){
+	*(dst + i * 3 + j) = src;
+}
+double getPtr(double* dst, int i, int j){
+	return *(dst + i * 3 + j);
+}
 void initialize() {
 
 	// initialize positions
-	r = (double**) malloc(N * 3 * sizeof(double));
-	v = (double**) malloc(N * 3 * sizeof(double));
-	a = (double**) malloc(N * 3 * sizeof(double));
+	r = new double[N * 3];
+	v = new double[N * 3];
+	a = new double[N * 3];
 	int n = int(ceil(pow(N, 1.0 / 3)));  // number of atoms in each direction
 	double a = L / n;                  // lattice spacing
 	int p = 0;                         // particles placed so far
@@ -31,16 +37,16 @@ void initialize() {
 		for (int y = 0; y < n; y++)
 			for (int z = 0; z < n; z++) {
 				if (p < N) {
-					r[p][0] = (x + 0.5) * a;
-					r[p][1] = (y + 0.5) * a;
-					r[p][2] = (z + 0.5) * a;
+					setPtr(r, p, 0, (x + 0.5) * a);
+					setPtr(r, p, 1, (y + 0.5) * a);
+					setPtr(r, p, 2, (z + 0.5) * a);
 				}
 				++p;
 			}
 	// initialize velocities
 	for (int p = 0; p < N; p++){
 		for (int i = 0; i < 3; i++){
-			v[p][i] = vMax * (2 * rand() / double(RAND_MAX) - 1);
+			setPtr(v, p, i, vMax * (2 * rand() / double(RAND_MAX) - 1));
 		}
 	}
 }
@@ -50,63 +56,30 @@ void SingleAcclerations(int i)
 		double rij[3];               // position of i relative to j
 		double rSqd = 0;
 		for (int k = 0; k < 3; k++) {
-			rij[k] = r[i][k] - r[j][k];
+			rij[k] = getPtr(r, i, k) - getPtr(r, j, k);
 			rSqd += rij[k] * rij[k];
 		}
 		double f = 24 * (2 * pow(rSqd, -7) - pow(rSqd, -4));
 		for (int k = 0; k < 3; k++) {
-			a[i][k] += rij[k] * f;
-			a[j][k] -= rij[k] * f;
+			setPtr(a, i, k, getPtr(a, i, k) + rij[k] * f);
+			setPtr(a, j, k, getPtr(a, j, k) - rij[k] * f);
 		}
 	}
 }
 //map
-void computeAccelerations(int index,int from,int to) {
+void computeAccelerations(int index, int from, int to) {
 	//计算加速度
 	memset(a, 0, N * 3 * sizeof(double));
-	//cout << index << ":(" << from << "," << to << ")" << endl;
 	for (int i = from; i < to; i++){    // loop over all distinct pairs i,j
 		SingleAcclerations(i);
 	}
-	//char filename[256] = { 0 };
-	//sprintf(filename, "data%d.txt", index);
-	//ofstream file(filename);
-	//for (int i = 0; i < N; i++){
-	//	for (int k = 0; k < N; k++){
-	//		file << a[i][k] << "\n";
-	//	}
-	//}
-	//file.close();
-	//cout << "Calc Acclerations end" << endl;
-}
-void hi(){
-	long area = N* (N - 1) / 2;
-	long splice = area / (5 - 1);
-	long from = N;
-	long to = 0;
-	//for (int i = 1; i < 8; i++){
-	long sum = 0;
-	int index = 0;
-	for (int j = N - 1; j > 0; j--){
-		sum += j;
-		if (sum >= splice || j == 1){
-			to = j;
-			long fromto[2] = { from, to };
-			printf("%d:%d,%d,%d\n", index, N - from, N - to, sum);
-			//MPI_Send(&fromto, MPI_LONG * 2, MPI_LONG, i, 1, MPI_COMM_WORLD);
-			from = j;
-			sum = 0;
-			index++;
-		}
-	}
-	getchar();
-	//}
 }
 double instantaneousTemperature() {
 	double sum = 0;
 	for (int i = 0; i < N; i++){
 		for (int k = 0; k < 3; k++){
-			sum += v[i][k] * v[i][k];
+			double tmp = getPtr(v, i, k);
+			sum += tmp * tmp;
 		}
 	}
 	return sum / (3 * (N - 1));
@@ -115,14 +88,14 @@ void velocityVerlet(double dt) {
 	computeAccelerations(0, 0, N - 1);
 	for (int i = 0; i < N; i++) {
 		for (int k = 0; k < 3; k++) {
-			r[i][k] += v[i][k] * dt + 0.5 * a[i][k] * dt * dt;
-			v[i][k] += 0.5 * a[i][k] * dt;
+			setPtr(r, i, k, getPtr(r, i, k) + getPtr(v, i, k) * dt + 0.5 * getPtr(a, i, k) * dt * dt);
+			setPtr(v, i, k, getPtr(v, i, k) + 0.5 * getPtr(a, i, k) * dt);
 		}
 	}
 	computeAccelerations(0, 0, N - 1);
 	for (int i = 0; i < N; i++){
 		for (int k = 0; k < 3; k++){
-			v[i][k] += 0.5 * a[i][k] * dt;
+			setPtr(v, i, k, getPtr(v, i, k) + getPtr(a, i, k) * dt);
 		}
 	}
 }
@@ -144,6 +117,7 @@ int main(int argc, char* argv [])
 			velocityVerlet(dt);
 			cout << instantaneousTemperature() << endl;
 		}
+		getchar();
 		return 0;
 	}
 	if (myid == 0){
@@ -153,7 +127,7 @@ int main(int argc, char* argv [])
 		for (int times = 0; times < TIMES; times++){
 			//b_CAST
 			for (int i = 1; i < numprocs; i++){
-				MPI_Send(&r[0][0], N * 3, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+				MPI_Send(r, N * 3, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
 			}
 			//send each block
 			long area = N* (N - 1) / 2;
@@ -176,19 +150,18 @@ int main(int argc, char* argv [])
 			//REDUCE
 			memset(a, 0, N * 3 * sizeof(double));
 			for (int i = 1; i < numprocs; i++){
-				double tmp[N][3] = { 0 };
+				double* tmp = new double[N * 3];
 				MPI_Status status;
-				MPI_Recv(&tmp[0][0], 3 * N, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &status);
-				for (int i = 0; i < N; i++){
-					for (int k = 0; k < 3; k++){
-						a[i][k] += tmp[i][k];
-					}
+				MPI_Recv(tmp, 3 * N, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &status);
+				for (int i = 0; i < N * 3; i++){
+					a[i] += tmp[i];
 				}
+				delete [] tmp;
 			}
 			for (int i = 0; i < N; i++) {
 				for (int k = 0; k < 3; k++) {
-					r[i][k] += v[i][k] * dt + 0.5 * a[i][k] * dt * dt;
-					v[i][k] += 0.5 * a[i][k] * dt;
+					setPtr(r, i, k, getPtr(r, i, k) + getPtr(v, i, k) * dt + 0.5 * getPtr(a, i, k) * dt * dt);
+					setPtr(v, i, k, getPtr(v, i, k) + 0.5 * getPtr(a, i, k) * dt);
 				}
 			}
 			//send each block again
@@ -210,18 +183,17 @@ int main(int argc, char* argv [])
 			//REDUCE
 			memset(a, 0, N * 3 * sizeof(double));
 			for (int i = 1; i < numprocs; i++){
-				double tmp[N][3] = { 0 };
+				double* tmp = new double[N * 3];
 				MPI_Status status;
-				MPI_Recv(&tmp, 3 * N, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &status);
-				for (int i = 0; i < N; i++){
-					for (int k = 0; k < 3; k++){
-						a[i][k] += tmp[i][k];
-					}
+				MPI_Recv(tmp, 3 * N, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &status);
+				for (int i = 0; i < N * 3; i++){
+					a[i] += tmp[i];
 				}
+				delete [] tmp;
 			}
 			for (int i = 0; i < N; i++){
 				for (int k = 0; k < 3; k++){
-					v[i][k] += 0.5 * a[i][k] * dt;
+					setPtr(v, i, k, getPtr(v, i, k) + getPtr(a, i, k) * dt);
 				}
 			}
 			file << instantaneousTemperature() << "\n";
@@ -231,19 +203,19 @@ int main(int argc, char* argv [])
 	else{
 		for (int times = 0; times < TIMES; times++){
 			MPI_Status status;
-			MPI_Recv(&r[0][0], N * 3, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
+			MPI_Recv(&r, N * 3, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
 
 			long fromto[2] = { 0, 0 };
 			MPI_Recv(&fromto[0], 2, MPI_LONG, 0, 2, MPI_COMM_WORLD, &status);
 
-			computeAccelerations(myid,fromto[0], fromto[1]);
-			MPI_Send(&a[0][0], N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+			computeAccelerations(myid, fromto[0], fromto[1]);
+			MPI_Send(&a, N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
 
 
 			MPI_Recv(&fromto[0], 2, MPI_LONG, 0, 2, MPI_COMM_WORLD, &status);
 
 			computeAccelerations(myid, fromto[0], fromto[1]);
-			MPI_Send(&a[0][0], N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
+			MPI_Send(&a, N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
 		}
 	}
 	free(r);
