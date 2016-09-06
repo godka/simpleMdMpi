71fcd128366e92b078490caf026b09507c97b525
godka <mythkast@hotmail.com>
2016-09-06 18:23:16 +0800

    cpp

--- SimpleMdMpi/SimpleMdMpi.cpp
+++ SimpleMdMpi/SimpleMdMpi.cpp
@@ -1,7 +1,6 @@
-﻿#include "mpi.h"
+﻿#include "mpi/mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
@@ -12,7 +11,7 @@
 
 using namespace std;
 #define TIMES 1000
-const int N = 640;         // number of particles
+const int N = 6400;         // number of particles
 double r[N][3];           // positions
 double v[N][3];           // velocities
 double a[N][3];           // accelerations
@@ -64,34 +63,10 @@ void SingleAcclerations(int i)
 void computeAccelerations(int index, int from, int to) {
     memset(&a[0][0], 0, N * 3 * sizeof(double));
     //cout << index << ":(" << from << "," << to << ")" << endl;
-    //#pragma omp parallel for
+//#pragma omp parallel for
     for (int i = from; i < to; i++){    // loop over all distinct pairs i,j
         SingleAcclerations(i);
     }
-}
-
-void hi(){
-    long area = N* (N - 1) / 2;
-    long splice = area / (5 - 1);
-    long from = N;
-    long to = 0;
-    //for (int i = 1; i < 8; i++){
-    long sum = 0;
-    int index = 0;
-    for (int j = N - 1; j > 0; j--){
-        sum += j;
-        if (sum >= splice || j == 1){
-            to = j;
-            long fromto[2] = { from, to };
-            //printf("%d:%d,%d,%d\n", index, N - from, N - to, sum);
-            //MPI_Send(&fromto, MPI_LONG * 2, MPI_LONG, i, 1, MPI_COMM_WORLD);
-            from = j;
-            sum = 0;
-            index++;
-        }
-    }
-    getchar();
-    //}
 }
 
 double instantaneousTemperature() {
@@ -135,7 +110,6 @@ void sendBlock(int numprocs)
             to = j;
             long fromto[2] = { N - from, N - to };
             MPI_Send(&fromto[0], 2, MPI_LONG, index, 2, MPI_COMM_WORLD);
-            cout << index << endl;
             from = j;
             sum = 0;
             index++;
@@ -171,17 +145,12 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Get_processor_name(processor_name, &namelen);
-    cout << "N = " << N << endl;
     initialize();
     if (numprocs == 1){
-        ofstream file("NoMPI.data");
-        FILE* fout = fopen("record-series.txt", "w+b");
+	cout << "N = " << N << endl;
+	double t1 = MPI_Wtime();
+	ofstream file("NoMPI.data");
         for (int times = 0; times < TIMES; times++){
-            for (int i = 0; i < N; i++)
-            {
-                fprintf(fout, "step %4d-1: %4d %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
-                    times, i, r[i][0], r[i][1], r[i][2], v[i][0], v[i][1], v[i][2], a[i][0], a[i][1], a[i][2]);
-            }
             computeAccelerations(0, 0, N - 1);
             for (int i = 0; i < N; i++) {
                 for (int k = 0; k < 3; k++) {
@@ -189,17 +158,7 @@ int main(int argc, char* argv[])
                     v[i][k] += 0.5 * a[i][k] * dt;
                 }
             }
-            for (int i = 0; i < N; i++)
-            {
-                fprintf(fout, "step %4d-2: %4d %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
-                    times, i, r[i][0], r[i][1], r[i][2], v[i][0], v[i][1], v[i][2], a[i][0], a[i][1], a[i][2]);
-            }
             computeAccelerations(0, 0, N - 1);
-            for (int i = 0; i < N; i++)
-            {
-                fprintf(fout, "step %4d-3: %4d %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
-                    times, i, r[i][0], r[i][1], r[i][2], v[i][0], v[i][1], v[i][2], a[i][0], a[i][1], a[i][2]);
-            }
             for (int i = 0; i < N; i++){
                 for (int k = 0; k < 3; k++){
                     v[i][k] += 0.5 * a[i][k] * dt;
@@ -207,32 +166,25 @@ int main(int argc, char* argv[])
             }
             file << instantaneousTemperature() << "\n";
         }
-        fclose(fout);
         file.close();
-        cout << "Press any key.\n";
-        getchar();
-        return 0;
+        double t2 = MPI_Wtime();
+	cout << "timespan:" << t2 - t1 << "s" << endl;
+	return 0;
     }
     if (myid == 0){
-        //int num = numprocs - 1;
-        ofstream file("T.data");
-        FILE* fout = fopen("record.txt", "w+b");
-        cout << myid<<"/" <<numprocs<< endl;
+        cout << "N = " << N << endl;
+	cout << "numprocs = " << numprocs << endl;
+	//int num = numprocs - 1;
+        double t1 = MPI_Wtime();
+	ofstream file("T.data");
         for (int i = 1; i < numprocs; i++){
             MPI_Send(&r[0][0], N * 3, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
         }
-        cout << "r sended " << endl;
         //send each block
         sendBlock(numprocs);
-        cout << "region sended " << endl;
         //REDUCE
         reduceBlock(numprocs);
-        cout << "a received " << endl;
         for (int times = 0; times < TIMES; times++){
-            //b_CAST
-            cout << instantaneousTemperature() << "\n";
-            
-            
             for (int i = 0; i < N; i++) {
                 for (int k = 0; k < 3; k++) {
                     r[i][k] += v[i][k] * dt + 0.5 * a[i][k] * dt * dt;
@@ -252,26 +204,22 @@ int main(int argc, char* argv[])
             file << instantaneousTemperature() << "\n";
 
         }
-        fclose(fout);
         file.close();
+    	double t2 = MPI_Wtime();
+        cout << "timespan:" << t2 - t1 << "s" << endl;
     }
     else{
-        cout << myid << "/" << numprocs << endl;
         MPI_Status status;
         MPI_Recv(&r[0][0], N * 3, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
-        cout << "r received "<< endl;
         long fromto[2] = { 0, 0 };
         MPI_Recv(&fromto[0], 2, MPI_LONG, 0, 2, MPI_COMM_WORLD, &status);
-        cout << "region received " << endl;
         computeAccelerations(myid, fromto[0], fromto[1]);
         MPI_Send(&a[0][0], N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
-        cout << "a sent " << endl;
 
         for (int times = 0; times < TIMES; times++){
      
             MPI_Recv(&r[0][0], N * 3, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
             //MPI_Recv(&fromto[0], 2, MPI_LONG, 0, 2, MPI_COMM_WORLD, &status);
-
             computeAccelerations(myid, fromto[0], fromto[1]);
             MPI_Send(&a[0][0], N * 3, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
         }
